fd41fff07fad5a29b1bba6ff9c28eb32ee042a9b
Ravi Kawade <rvikwd3@outlook.com>
2018-07-14 19:48:36 +0530

    Ping Pong Example http://mpitutorial.com/tutorials/mpi-send-and-receive/

--- Assignment5.c
+++ Assignment5.c
@@ -1,9 +1,9 @@
 #include <mpi.h>
 #include <stdio.h>
 
-using namespace std;
+int main(int argc, char** argv){
+    const int PING_PONG_LIMIT = 5;
 
-int main(){
     MPI_Init(NULL, NULL);
 
     int world_size;
@@ -12,11 +12,25 @@ int main(){
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+    if(world_size != 2){
+        fprintf(stderr, "World size must only 2 processes\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
 
-    printf("Hello world\nProcessor:\t%s\nRank:\t%d\tout of\t%d\tprocessors\n", processor_name, world_rank, world_size);
+    int ping_pong_count;
+    int partner_rank = (world_rank + 1) % 2;
+
+    while(ping_pong_count < PING_PONG_LIMIT){
+        if(world_rank == ping_pong_count % 2){
+            ping_pong_count++;
+
+            MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+            printf("P%d Ping %d to %d\n", world_rank, ping_pong_count, partner_rank);
+        }else{
+            MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("P%d Pong %d from %d\n", world_rank, ping_pong_count, partner_rank);
+        }
+    }
 
     MPI_Finalize();
 
