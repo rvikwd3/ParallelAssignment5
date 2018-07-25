e05222bbfe8f5b98cbed29b7bc4120dc5dde6097
Ravikiran Kawade <ravikiran.kawade@outlook.com>
2018-07-25 15:56:31 +0530

    Replaced naive bit length with DeBruijn method of Highest Bit Order

--- SumOfNumbers.c
+++ SumOfNumbers.c
@@ -14,12 +14,30 @@ Addition of n nodes within p groups
 
 #define N 8
 
+unsigned int getBinaryLength(unsigned int v){
+    //DeBruijn Method of Highest Bit Position
+    //http://supertech.csail.mit.edu/papers/debruijn.pdf
+    static const int MultiplyDeBruijnBitPosition[32] =
+    {
+        0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+        8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
+    };
+
+    v |= v >> 1; // first round down to one less than a power of 2
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+
+    return MultiplyDeBruijnBitPosition[(unsigned int)( v * 0x07C4ACDDU ) >> 27];
+}
+
 int main(){
     MPI_Init(NULL, NULL);
 
-    int world_size;
+    unsigned int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    int world_rank;
+    unsigned int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
 
@@ -48,6 +66,10 @@ int main(){
     int *node_data= (int*)malloc(sizeof(int));
     assert(node_data!= NULL);
 
+    //Check number of binary digits
+    int binary_digits = getBinaryLength(world_size); 
+    if(world_rank == 0){ printf("Maximum bit size:\t%d\n", binary_digits); }
+
     //Scatter number array from P0 to other processes
     MPI_Barrier(MPI_COMM_WORLD);
     if(world_rank==0){ printf("First barrier reached\n"); }
@@ -58,10 +80,18 @@ int main(){
     //Current process and its data
     printf("P%d: [%d]\n", world_rank, *node_data);
 
-    //LSB complement
-    int bit_complement = world_rank ^ 1UL << 0;
-    printf("P%d: LSB bit complement %d\n", world_rank, bit_complement);
+    //Ready the receiving buffer
+    unsigned int* recv_buffer = (int*)malloc(sizeof(int));
+    assert(recv_buffer != NULL);
 
+    for(int i=0; i < binary_digits; i++){
+        //LSB complement
+        int bit_complement = world_rank ^ 1UL << i;
+        printf("P%d: LSB bit complement %d\n", world_rank, bit_complement);
+
+        // MPI_Ssend(node_data, 1, MPI_INT, bit_complement, 0, MPI_COMM_WORLD);
+
+    }
     free(node_data);
 
     MPI_Finalize();
