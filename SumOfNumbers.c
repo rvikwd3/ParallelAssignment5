94ce78ca23de58c99aef26bc3478453d5b365cb0
Ravi Kawade <rvikwd3@outlook.com>
2018-07-25 12:06:59 +0530

    Added MPI_Scatter Changed filename

---
+++ SumOfNumbers.c
@@ -0,0 +1,61 @@
+/*
+Ravikiran Kawade
+Parallel Assignment 5
+
+Addition of n nodes within p groups
+20180724
+*/
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+
+#define N 8
+
+int main(int argc, char** argv){
+
+    srand(time(NULL));
+
+    MPI_Init(NULL, NULL);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    if(world_size > N){
+        fprintf(stderr, "At most use %d processes only", N);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    int input_array[world_size];
+
+    if(world_rank == 0){
+        //Get random numbers
+        printf("Randomized Array:\t");
+        for(int i=0; i < world_size; i++){
+            input_array[i] = rand() % 100;
+            printf(" %d", input_array[i]);
+        }
+        printf("\n");
+
+    }
+
+    //Create integer buffer to receive number
+    int *number_buffer = (int*)malloc(sizeof(int));
+    assert(number_buffer != NULL);
+
+    //Scatter number array from P0 to other processes
+    printf("P%d: Scattered Input Array\n", world_rank);
+    MPI_Scatter(input_array, 1, MPI_INT, number_buffer, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    printf("P%d: [%d]\n", world_rank, *number_buffer);
+
+    free(number_buffer);
+
+    MPI_Finalize();
+
+    return 0;
+}
